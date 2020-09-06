17390a06904b13b6c9a2592028d60f9d238482c3
wlsvy <hubjpkim@gmail.com>
2020-09-07 00:14:23 +0900

    Update Scene

---
+++ Ladybug3D/Libraries/Renderer/Scene.cpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "Scene.hpp"
+
+namespace Ladybug3D {
+
+	Scene::Scene()
+		: m_WorldTransform(std::make_shared<Transform>()
+	{
+	}
+	Scene::~Scene()
+	{
+	}
+
+	void Scene::Initialize()
+	{
+		/*Core::Find<GameObject>("X_Bot")->GetRenderInfo().Anim->SetClip(Core::Find<AnimationClip>("X_Bot_Idle"));
+		Core::Find<GameObject>("X_Bot")->GetRenderInfo().Anim->Play();
+
+		{
+			auto sphere = Core::CreateInstance<GameObject>("Cube");
+			sphere->GetTransform().SetPosition(3.0f, 5.0f, 2.9f);
+			sphere->GetRenderInfo().SetModel(Core::Find<Model>("Box"));
+			sphere->GetRenderInfo().SetMaterial(0, Core::Find<SharedMaterial>("AluminiumInsulator"));
+		}
+		{
+			auto sphere = Core::CreateInstance<GameObject>("Furry");
+			sphere->GetTransform().SetPosition(2.0f, 5.0f, -3.f);
+			sphere->GetTransform().SetScale(3.0f, 3.0f, 3.f);
+			sphere->GetRenderInfo().SetModel(Core::Find<Model>("torus"));
+			sphere->GetRenderInfo().SetMaterial(0, Core::Find<SharedMaterial>("FurTest"));
+			sphere->GetRenderInfo().SetShaderState(0, Core::Find<ShaderState>("FurMesh"));
+		}
+
+		m_MainCam = Core::CreateInstance<Camera>();
+		m_MainCam->GetTransform().SetPosition(-2.6f, 11.3f, -8.6f);
+		m_MainCam->GetTransform().SetRotation(40.0f, 30.0f, 0.0f);
+		m_MainCam->SetProjectionValues(
+			90.0f,
+			Engine::Get().GetGraphics().GetWindowWidth() / Engine::Get().GetGraphics().GetWindowHeight(),
+			0.1f,
+			1000.0f);
+		m_MainCam->AddComponent<CamMove>();*/
+	}
+
+	void Scene::Update()
+	{
+		m_WorldTransform->UpdateMatrix(DirectX::XMMatrixIdentity(), DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f));
+		m_MainCam->UpdateView();
+	}
+}
---
+++ Ladybug3D/Libraries/Renderer/Scene.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "Object.hpp"
+
+namespace Ladybug3D {
+
+
+	class Scene : public Object {
+	public:
+		static constexpr float MAX_SIZE = 9999.0f;
+		static constexpr float MIN_SIZE = -9999.0f;
+
+		Scene();
+		~Scene();
+	private:
+		void Initialize();
+
+		void Update();
+
+		std::shared_ptr<Transform> GetWorldTransform() const { return m_WorldTransform; }
+		std::shared_ptr<Camera> GetMainCam() const { return m_MainCam; }
+		std::weak_ptr<Transform> GetGuiSelected() const { return m_GuiSelectedObj; }
+
+	private:
+		void AwakeGameObject();
+
+		static bool s_IsInitialized;
+
+		std::shared_ptr<Transform> m_WorldTransform;
+		std::shared_ptr<Camera> m_MainCam;
+
+		std::weak_ptr<Transform> m_GuiSelectedObj;
+	};
+}
