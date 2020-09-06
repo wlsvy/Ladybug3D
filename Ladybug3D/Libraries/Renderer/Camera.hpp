8e76e1a6336cfb312674a268f648f67363e32ae7
wlsvy <hubjpkim@gmail.com>
2020-09-06 23:34:04 +0900

    Update Object

---
+++ Ladybug3D/Libraries/Renderer/Camera.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <Util.hpp>
+
+namespace Ladybug3D {
+	class Camera : public Object {
+	public:
+	private:
+	};
+}
---
+++ Ladybug3D/Libraries/Renderer/Object.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "Object.hpp"
+
+using namespace Ladybug3D;
+
+uint GetNewObjectID()
+{
+	static uint s_Id = 0;
+	return s_Id++;
+}
+
+
+Object::Object()
+	: Name("Object")
+	, m_Id(GetNewObjectID())
+{
+}
+
+Object::Object(const std::string& name)
+	: Name(name)
+	, m_Id(GetNewObjectID())
+{
+}
+
+Object::Object(const Object& obj)
+	: Name(obj.Name)
+	, m_Id(GetNewObjectID())
+{
+}
+
+Object::Object(Object&& obj) noexcept
+	: Name(std::move(obj.Name))
+	, m_Id(std::move(obj.m_Id))
+{
+}
+
+Ladybug3D::Object::~Object()
+{
+}
+
+Object& Object::operator=(const Object& obj)
+{
+	Name = obj.Name;
+	return *this;
+}
