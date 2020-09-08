e715b75ea79b469ab03a80b1ef42c687634303bc
wlsvy <hubjpkim@gmail.com>
2020-09-08 20:23:59 +0900

    Create ResourceManager

---
+++ Ladybug3D/Libraries/Renderer/ResourceManager.cpp
@@ -0,0 +1,12 @@
+#include "ResourceManager.hpp"
+
+using namespace std;
+
+namespace Ladybug3D {
+	ResourceManager::ResourceManager()
+	{
+	}
+	ResourceManager::~ResourceManager()
+	{
+	}
+}
---
+++ Ladybug3D/Libraries/Renderer/ResourceManager.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <memory>
+
+namespace Ladybug3D {
+	class ResourceManager {
+	public:
+		ResourceManager();
+		~ResourceManager();
+	};
+}
