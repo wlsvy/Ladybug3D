2a25d3d6217a42c1f98e307a196c3c192f516540
wlsvy <hubjpkim@gmail.com>
2020-09-08 17:31:36 +0900

    Update Constant Buffer

--- Ladybug3D/Libraries/D3D12/D3D12_ConstantBuffer.hpp
+++ Ladybug3D/Libraries/D3D12/D3D12_ConstantBuffer.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "D3D12_Define.hpp"
 #include "D3D12_Resource.hpp"
 #include "d3dx12.h"
 
@@ -9,7 +8,6 @@ namespace Ladybug3D::D3D12 {
 	class ConstantBuffer : public Resource {
 	public:
 		ConstantBuffer(ID3D12Device* device);
-		~ConstantBuffer();
 		void CreateConstantBufferView(ID3D12Device* device, D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor);
 		T* Data;
 	};
@@ -17,23 +15,12 @@ namespace Ladybug3D::D3D12 {
 	template<typename T>
 	inline ConstantBuffer<T>::ConstantBuffer(ID3D12Device* device)
 	{
-		ThrowIfFailed(device->CreateCommittedResource(
-			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
-			D3D12_HEAP_FLAG_NONE,
-			&CD3DX12_RESOURCE_DESC::Buffer(sizeof(T)),
-			D3D12_RESOURCE_STATE_GENERIC_READ,
-			nullptr,
-			IID_PPV_ARGS(m_Resource.GetAddressOf())
-		));
+		CreateBuffer(device, sizeof(T), D3D12_HEAP_TYPE_UPLOAD);
 
 		CD3DX12_RANGE readRange(0, 0);
 		ThrowIfFailed(m_Resource->Map(0, &readRange, reinterpret_cast<void**>(&Data)));
 	}
-	template<typename T>
-	inline ConstantBuffer<T>::~ConstantBuffer()
-	{
-		Data = nullptr;
-	}
+
 	template<typename T>
 	inline void ConstantBuffer<T>::CreateConstantBufferView(ID3D12Device* device, D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor)
 	{
