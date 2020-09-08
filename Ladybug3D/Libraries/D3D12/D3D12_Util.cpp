3591aad1ad9a94b27c002bf041606b78f7a4e10e
wlsvy <hubjpkim@gmail.com>
2020-09-08 23:01:38 +0900

    Update D3d12_Util

--- Ladybug3D/Libraries/D3D12/D3D12_Util.cpp
+++ Ladybug3D/Libraries/D3D12/D3D12_Util.cpp
@@ -7,7 +7,7 @@ namespace Ladybug3D::D3D12 {
 	std::string HrToString(HRESULT hr)
 	{
 		char s_str[64] = {};
-		sprintf_s(s_str, "HRESULT of 0x%08X", static_cast<UINT>(hr));
+		sprintf_s(s_str, "HRESULT of 0x%08X  ", static_cast<UINT>(hr));
 		return std::string(s_str);
 	}
 
--- Ladybug3D/Libraries/D3D12/D3D12_Util.hpp
+++ Ladybug3D/Libraries/D3D12/D3D12_Util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdexcept>
 #include <Windows.h>
+#include <iostream>
 #include "D3D12_Define.hpp"
 
 namespace Ladybug3D::D3D12 {
@@ -9,18 +10,20 @@ namespace Ladybug3D::D3D12 {
 
     inline void ThrowIfFailed(HRESULT hr)
     {
-        if (FAILED(hr))
-        {
+        if (FAILED(hr)) {
             throw std::runtime_error(HrToString(hr));
         }
     }
-    inline void ThrowIfFailed(HRESULT hr, const char* message)
+    inline void ThrowIfFailed(HRESULT hr, const char* messageOnFail, const char* messageOnSuccess = nullptr)
     {
-        if (FAILED(hr))
-        {
-            throw std::runtime_error(HrToString(hr) + message);
+        if (FAILED(hr)) {
+            throw std::runtime_error(messageOnFail);
+        }
+        else if(messageOnSuccess != nullptr) {
+            std::cout << messageOnSuccess << std::endl;
         }
     }
+
 
 	// Assign a name to the object to aid with debugging.
 #if defined(_DEBUG) || defined(DBG)
@@ -33,5 +36,4 @@ namespace Ladybug3D::D3D12 {
 
 #define NAME_D3D12_OBJECT(x) SetName((x).Get(), L#x)
 #define NAME_D3D12_OBJECT_INDEXED(x, n) SetNameIndexed((x)[n].Get(), L#x, n)
-
 }
